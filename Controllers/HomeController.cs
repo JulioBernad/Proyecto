4401ef33f0f9fc9a4bd0af83a8e5fef0449b9417
Julio Bernad Castejon <jbernad@hiberus.local>
2017-03-28 14:25:30 +0200

    Signed-off-by: Julio Bernad Castejon <jbernad@hiberus.local>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ProyectoEntities db = new ProyectoEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LogController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+using Proyecto.Controllers;
+
+namespace Proyecto.Controllers
+{
+    public class LogController : Controller
+    {
+        private ProyectoEntities db = new ProyectoEntities();
+
+        // GET: /Log/
+        public ActionResult Index()
+        {
+            
+            return View(db.Usuarios.ToList());
+        }
+
+        // GET: /Log/
+        public ActionResult List(IEnumerable<int> usuarios)
+        {
+            List<Usuarios> user = new List<Usuarios>();
+            user = db.Usuarios.ToList(); ;
+            return View(user);
+        }
+
+        // GET: /Log/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: /Log/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Log/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="idUsuario,nomUsuario,apellido,correo,contrasena")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        // GET: /Log/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: /Log/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="idUsuario,nomUsuario,apellido,correo,contrasena")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuarios);
+        }
+
+        // GET: /Log/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: /Log/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuarios usuarios = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
